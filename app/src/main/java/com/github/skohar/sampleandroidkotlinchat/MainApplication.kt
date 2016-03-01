48f62d778e77542b7238eb85f99f74d431f0623d
skohar <skohar@users.noreply.github.com>
2016-03-02 06:47:10 +0900

    timber halfway

--- app/src/main/java/com/github/skohar/sampleandroidkotlinchat/MainApplication.kt
+++ app/src/main/java/com/github/skohar/sampleandroidkotlinchat/MainApplication.kt
@@ -1,13 +1,41 @@
 package com.github.skohar.sampleandroidkotlinchat
 
 import android.app.Application
+import android.util.Log
 import com.squareup.leakcanary.LeakCanary
 import hugo.weaving.DebugLog
+import timber.log.Timber
 
 class MainApplication : Application() {
 
     @DebugLog override fun onCreate() {
         super.onCreate()
         LeakCanary.install(this)
+        //        if (BuildConfig.DEBUG) {
+        //            Timber.plant(Timber.DebugTree());
+        //        } else {
+        // debuglogが見えない
+        //        Timber.plant(CrashReportingTree());
+        //        Timber.e("", Exception())
+        //        }
+
+    }
+
+    /** A tree which logs important information for crash reporting. */
+    private class CrashReportingTree : Timber.Tree () {
+        override fun log(priority: Int, tag: String, message: String, t: Throwable) {
+            if (priority == Log.VERBOSE || priority == Log.DEBUG) {
+                return
+            } else {
+                FakeCrashLibrary.log(priority, tag, message)
+                t.let { t ->
+                    if (priority == Log.ERROR) {
+                        FakeCrashLibrary.logError(t)
+                    } else if (priority == Log.WARN) {
+                        FakeCrashLibrary.logWarning(t)
+                    }
+                }
+            }
+        }
     }
 }
