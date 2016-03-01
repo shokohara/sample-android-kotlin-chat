53ffe0763d5932df0cc29db66325565aafdcbdb4
skohar <skohar@users.noreply.github.com>
2016-03-01 23:06:15 +0900

    Add hugo

--- app/src/main/java/com/github/skohar/sampleandroidkotlinchat/MainApplication.kt
+++ app/src/main/java/com/github/skohar/sampleandroidkotlinchat/MainApplication.kt
@@ -2,9 +2,11 @@ package com.github.skohar.sampleandroidkotlinchat
 
 import android.app.Application
 import com.squareup.leakcanary.LeakCanary
+import hugo.weaving.DebugLog
 
-class MainApplication : Application(){
-    override fun onCreate() {
+class MainApplication : Application() {
+
+    @DebugLog override fun onCreate() {
         super.onCreate()
         LeakCanary.install(this)
     }
